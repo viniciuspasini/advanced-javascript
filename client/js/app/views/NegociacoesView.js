7b507188f9dc630ae9b1cf6c56770878e3517d43
Vincius Pasini <vinicius.pasini@datweb.com.br>
2021-06-05 01:12:23 -0300

    aula 05

---
+++ client/js/app/views/NegociacoesView.js
@@ -0,0 +1,45 @@
+class NegociacaoView{
+
+    constructor(elemento){
+        this._elemento = elemento;
+    }
+
+    _template(model){
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                        <th>VOLUME</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                ${model.negociacoes.map((negociacao) => `
+                    <tr>
+                        <td>${DateHelper.dataParaTexto(negociacao.data)}</td>
+                        <td>${negociacao.quantidade}</td>
+                        <td>${negociacao.valor}</td>
+                        <td>${negociacao.volume}</td>
+                    </tr>
+                `).join('')}
+                </tbody>
+                
+                <tfoot>
+                    <tr>
+                        <td colspan='3'></td>
+                        <td>
+                            ${model.negociacoes.reduce((total, negociacao) => total + negociacao.volume, 0)}
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        `;
+    }
+
+    update(model){
+        return this._elemento.innerHTML = this._template(model);
+    }
+}
